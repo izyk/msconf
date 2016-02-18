dcbe63199133760924b000df669f7740bef0319b
root <root@hs2.izyk.ru>
2016-02-18 23:56:18 +0300

    Add ip6tables.

--- iptbl/iptbl.sh
+++ iptbl/iptbl.sh
@@ -2,27 +2,29 @@
 # Version info - $Id: iptbl.sh,v 1.4 2016-02-12 16:59:09+03 ilia Exp ilia $
 
 IPTABLES=/sbin/iptables
+IP6TABLES=/sbin/ip6tables
 SYSCTL=/sbin/sysctl
 
 # ACTIVE ZONES
 ZPATH=$(pwd)/zones
 ZONES="eth00 eth01 lbr0"
 
-zone_init () {
-# IPV4 ZONE
+zone_init() {
+# IPV4 IPV6 ZONE
 
 indev=false
 in4ip=no_address
-masq=no		# MASQUERADE packets to this network
+in6net=no_address
+masq=no		# MASQUERADE packets to this network only IPV4
 trust=no	# ACCEPT all packets from this network
 forward=no	# FORWARD all packets from this network
 
 # INPUT
-I4U=""	# UDP
-I4T=""	# TCP
+INU=""	# UDP
+INT=""	# TCP
 
 # SECURITY ATTENTION!
-# OPEN TWO PORTS ON INTERFACE (sport,dport) maybe vulnerability!!! Don't use on external.
+# OPEN WIDE TWO PORTS sport and dport - maybe vulnerability!!! Don't use on external.
 # LOCAL REDIRECTION - example "source,destination 1015,2012" space separator
 I4UR="" # UDP
 I4TR=""	# TCP
@@ -34,37 +36,37 @@ I4TR=""	# TCP
 F4U=""	# UDP
 F4T=""	# TCP
 
-# IPV4 ZONE END
+# IPV4 IPV6 ZONE END
 }
 
-get_param () {
+get_param() {
   eval GET_PARAM=\$$1
 }
-parse_comma () {
+parse_comma() {
   PARSE_COMMA=$(echo $1 | tr "," ' ')
 }
 
-build_local_redirect () {
+build_local_redirect() {
   # echo $1 zone
   # echo $2 proto
   # echo $3 sport
   # echo $4 dport
-  $IPTABLES -t nat    -A ${1}_nat   -p $2 --dport $3 -j REDIRECT --to-ports $4
-  $IPTABLES -t filter -A ${1}_input -p $2 --dport $4 -j ACCEPT
+  $IPTABLES -t nat    -A NAT_${1}   -p $2 --dport $3 -j REDIRECT --to-ports $4
+  $IPTABLES -t filter -A INPUT_${1} -p $2 --dport $4 -j ACCEPT
 }
 
-build_forward_rule () {
+build_forward_rule() {
   # echo $1 zone
   # echo $2 proto
   # echo $3 sport
   # echo $4 dstip
   # echo $5 dport
-  $IPTABLES -t nat    -A ${1}_nat     -p $2       --dport $3 -j DNAT --to $4:$5
-  $IPTABLES -t filter -A ${1}_forward -p $2 -d $4 --dport $5 -j ACCEPT
+  $IPTABLES -t nat    -A NAT_${1}     -p $2       --dport $3 -j DNAT --to $4:$5
+  $IPTABLES -t filter -A FORWARD_${1} -p $2 -d $4 --dport $5 -j ACCEPT
 }
 
 # ( "ZONE" "NAME OF PARAM - ex: F4U" "PROTO" )
-parse_4forward () {
+parse_4forward() {
   get_param $2
   for fpar in $GET_PARAM
   do
@@ -74,7 +76,7 @@ parse_4forward () {
 }
 
 # ( "ZONE" "NAME OF PARAM - ex: F4UR" "PROTO" )
-local_4redirect () {
+local_4redirect() {
   get_param $2
   for fpar in $GET_PARAM
   do
@@ -84,34 +86,46 @@ local_4redirect () {
 }
 
 # ( "ZONE" "PORTS" "PROTO" )
-in_ports () {
+in_ports() {
   if [ x"$2" != x"" ]; then
-    $IPTABLES -t filter -A ${1}_input -p $3 -m multiport --dports $2 -j ACCEPT
+    $IPTABLES -t filter -A INPUT_${1} -p $3 -m multiport --dports $2 -j ACCEPT
+    $IP6TABLES -t filter -A INPUT_${1} -p $3 -m multiport --dports $2 -j ACCEPT
   fi
 }
 
-load_zones () {
+load_zones() {
   for zone in $ZONES
   do
     zone_init
     . $ZPATH/$zone
-    $IPTABLES -t nat    -N ${zone}_nat
-    $IPTABLES -t filter -N ${zone}_input
-    $IPTABLES -t filter -N ${zone}_forward
-    $IPTABLES -t nat    -A PREROUTING -i $indev -d $in4ip -j ${zone}_nat
-    $IPTABLES -t filter -A INPUT      -i $indev -d $in4ip -j ${zone}_input
-    $IPTABLES -t filter -A FORWARD    -i $indev -j ${zone}_forward # no $in4ip maybe vulnerability without "net.ipv4.conf.all.rp_filter = 1"
-    if [ $masq == 'yes' ]; then
-      $IPTABLES -t nat    -A POSTROUTING -o $indev -m addrtype ! --src-type LOCAL  -j MASQUERADE
-    fi
+
     if [ $trust == 'yes' ]; then
       $IPTABLES -t filter -A INPUT   -i $indev -j ACCEPT
+      $IP6TABLES -t filter -A INPUT   -i $indev -j ACCEPT
     fi
     if [ $forward == 'yes' ]; then
       $IPTABLES -t filter -A FORWARD -i $indev -j ACCEPT
+      $IP6TABLES -t filter -A FORWARD -i $indev -j ACCEPT
     fi
-    in_ports $zone "$I4U" udp
-    in_ports $zone "$I4T" tcp
+
+    $IPTABLES -t nat    -N NAT_${zone}
+    $IPTABLES -t filter -N INPUT_${zone}
+    $IPTABLES -t filter -N FORWARD_${zone}
+    $IPTABLES -t nat    -A PREROUTING -i $indev -d $in4ip -j NAT_${zone}
+    $IPTABLES -t filter -A INPUT      -i $indev -d $in4ip -j INPUT_${zone}
+    $IPTABLES -t filter -A FORWARD    -i $indev -j FORWARD_${zone} # no $in4ip maybe vulnerability without "net.ipv4.conf.all.rp_filter = 1"
+    if [ $masq == 'yes' ]; then
+      $IPTABLES -t nat    -A POSTROUTING -o $indev -m addrtype ! --src-type LOCAL  -j MASQUERADE
+    fi
+
+    $IP6TABLES -t filter -N INPUT_${zone}
+    $IP6TABLES -t filter -N FORWARD_${zone}
+    $IP6TABLES -t filter -A INPUT      -i $indev -d $in6net -j INPUT_${zone}
+    $IP6TABLES -t filter -A FORWARD    -i $indev -d $in6net -j FORWARD_${zone}
+
+    in_ports $zone "$INU" udp
+    in_ports $zone "$INT" tcp
+
     local_4redirect $zone I4UR udp
     local_4redirect $zone I4TR tcp
     parse_4forward $zone F4U udp
@@ -119,26 +133,38 @@ load_zones () {
   done
 }
 
-create_rules () {
-  $IPTABLES -t filter -N CHECK-NEW-TCP
-  $IPTABLES -t filter -A CHECK-NEW-TCP -p tcp --syn -j RETURN
-  $IPTABLES -t filter -A CHECK-NEW-TCP -p tcp ! --tcp-flags SYN,ACK,FIN,RST SYN,ACK -j DROP
-  $IPTABLES -t filter -A CHECK-NEW-TCP -p tcp -j REJECT --reject-with tcp-reset
+# ( $IPTABLES )
+create_default(){
+  local iptbl="$1"
+  $iptbl -t filter -N CHECK-NEW-TCP
+  $iptbl -t filter -A CHECK-NEW-TCP -p tcp --syn -j RETURN
+  $iptbl -t filter -A CHECK-NEW-TCP -p tcp ! --tcp-flags SYN,ACK,FIN,RST SYN,ACK -j DROP
+  $iptbl -t filter -A CHECK-NEW-TCP -p tcp -j REJECT --reject-with tcp-reset
 
-  $IPTABLES -t filter -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-  $IPTABLES -t filter -A INPUT -m state ! --state NEW -j DROP
-  $IPTABLES -t filter -A INPUT -p tcp -j CHECK-NEW-TCP
+  $iptbl -t filter -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+  $iptbl -t filter -A INPUT -m state ! --state NEW -j DROP
+  $iptbl -t filter -A INPUT -p tcp -j CHECK-NEW-TCP
+  $iptbl -t filter -A INPUT -i lo -j ACCEPT
+
+  $iptbl -t filter -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+  $iptbl -t filter -A FORWARD -m state ! --state NEW -j DROP
+  $iptbl -t filter -A FORWARD -p tcp -j CHECK-NEW-TCP
+}
+
+create_rules() {
+  create_default "$IPTABLES"
+  create_default "$IP6TABLES"
+    
   $IPTABLES -t filter -A INPUT -p icmp -j ACCEPT
-  $IPTABLES -t filter -A INPUT -i lo -j ACCEPT
-
-  $IPTABLES -t filter -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
-  $IPTABLES -t filter -A FORWARD -m state ! --state NEW -j DROP
-  $IPTABLES -t filter -A FORWARD -p tcp -j CHECK-NEW-TCP
+  $IP6TABLES -t filter -A INPUT -p ipv6-icmp -j ACCEPT
 
   load_zones
 
   $IPTABLES -t filter -A INPUT -j REJECT --reject-with icmp-host-prohibited
   $IPTABLES -t filter -A FORWARD -j REJECT --reject-with icmp-host-prohibited
+
+  $IP6TABLES -t filter -A INPUT -j REJECT --reject-with icmp6-adm-prohibited
+  $IP6TABLES -t filter -A FORWARD -j REJECT --reject-with icmp6-adm-prohibited
 }
 
 usage() {
@@ -146,22 +172,29 @@ usage() {
     exit $1
 }
 
+# ( $IPTABLES )
+reset_tables() {
+  local iptbl="$1"
+  $iptbl -F
+  $iptbl -t raw -F
+  $iptbl -t nat -F
+  $iptbl -t mangle -F
+  $iptbl -Z
+  $iptbl -t raw -Z
+  $iptbl -t nat -Z
+  $iptbl -t mangle -Z
+  $iptbl -X
+  $iptbl -t raw -X
+  $iptbl -t nat -X
+  $iptbl -t mangle -X
+  $iptbl -P INPUT ACCEPT
+  $iptbl -P OUTPUT ACCEPT
+  $iptbl -P FORWARD ACCEPT
+}
+
 stop() {
-  $IPTABLES -F
-  $IPTABLES -t raw -F
-  $IPTABLES -t nat -F
-  $IPTABLES -t mangle -F
-  $IPTABLES -Z
-  $IPTABLES -t raw -Z
-  $IPTABLES -t nat -Z
-  $IPTABLES -t mangle -Z
-  $IPTABLES -X
-  $IPTABLES -t raw -X
-  $IPTABLES -t nat -X
-  $IPTABLES -t mangle -X
-  $IPTABLES -P INPUT ACCEPT
-  $IPTABLES -P OUTPUT ACCEPT
-  $IPTABLES -P FORWARD ACCEPT
+  reset_tables "$IPTABLES"
+  reset_tables "$IP6TABLES"
 }
 
 start() {
@@ -184,6 +217,7 @@ then
   if [ $# -eq 2 -a $2 = 'test' ]
   then
     IPTABLES="echo iptables"
+    IP6TABLES="echo ip6tables"
     SYSCTL="echo sysctl"
   else
     usage 1
