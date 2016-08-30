1e98cf87981a7483486e2f654f9d661bb0488563
root <root@hs2.izyk.ru>
2016-08-30 18:13:38 +0300

    Refactoring. Replace comma to semicolon in fields separator.

--- iptbl/iptbl.sh
+++ iptbl/iptbl.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-# Version info - $Id: iptbl.sh,v 1.1 2016-08-26 19:45:32+03 root Exp root $
+# Name Convenstions
+# Constants - CONST ...
+# Variable - var1, var_two ...
 
 IPTABLES=/sbin/iptables
 IP6TABLES=/sbin/ip6tables
@@ -11,7 +13,8 @@ ZONES=$(ls ${ZPATH})
 # GLOBAL PARAMETERS
 FMASK=0xFF00		# FORWARD MASK
 FFLAG=0x8000		# FORWARD FLAG
-forward_mark=$FFLAG	# Mark packets as forward max 127.
+FOINC=0x0100		# FORWARD INCREMENT
+forward_mark=$FFLAG	# Mark packets as forward max 128.
 
 # Set default (some times useless) values for all parameters.
 # In zones files (see above) this should be reinit with correct value.
@@ -19,43 +22,40 @@ zone_init() {
 indev=false
 in4ip=no_address
 in6net=no_address
-# SNAT (in4ip) packets to this network from this IPV4s. Must be before MASQUARADE
-snat=no		# ( x.x.x.x[/mask][,y.y.y.y]... )
+# SNAT outgoing packets to in4ip address from this IPV4s source. Must be before MASQUARADE
+snat=no		# ( x.x.x.x[/mask][;y.y.y.y]... )
 masq=no		# MASQUERADE packets to this network only IPV4
 trust=no	# ACCEPT all packets from this network
 forward=no	# FORWARD all packets from this network
 
-# INPUT
-INU=""	# UDP
-INT=""	# TCP
+# INPUT "inport1,inport2..."
+# example "22,1368,8880"
+udpin=""	# UDP
+tcpin=""	# TCP
 
-# SECURITY ATTENTION!
-# OPEN WIDE TWO PORTS sport and dport - maybe vulnerability!!! Don't use on external.
-# LOCAL REDIRECTION "input_port,destination_port"
-# example "1015,2012 1016,2013" pair separate with space
-I4UR="" # UDP
-I4TR=""	# TCP
+# LOCAL REDIRECTION "input_port;destination_port" or not change destination port if multi source
+# example "1015;2012 1016;2013 21,47,9999;multi" params separate with space, ports with semicolon
+udprin=""	# UDP
+tcprin=""	# TCP
 
-# SECURITY ATTENTION!
-# maybe vulnerability without "net.ipv4.conf.all.rp_filter = 1"
-# without above can OPEN TWO PORTS ON INTERFACE (sport,dport).
-# FORWARDING - example "sport,dstip,dport 10080,192.168.40.40,20008" space separator
-F4U=""	# UDP
-F4T=""	# TCP
+# FORWARDING - example "sport;dstip;dport 10080;192.168.40.40;20008 10081,20080;192.168.40.40;multi"
+# params separate with space, ports and ips with semicolon. In last example no port changes.
+udp4forward=""	# UDP
+tcp4forward=""	# TCP
 
-# IPV6 FORWARDING - example "dstip,dport fe00::1,578" space separator
-F6T=""	# TCP
+# IPV6 FORWARDING - example "dstip;dport fe00::1;578" semicolon separator
+tcp6forward=""	# TCP
 
 } # END the default value init.
 
-get_param() {
-  eval GET_PARAM=\$$1
+get_variable() {
+  eval get_variable_result=\$$1
 }
-parse_comma() {
-  PARSE_COMMA=$(echo $1 | tr "," ' ')
+split_variable() {
+  split_variable_result=$(echo $1 | tr ";" ' ')
 }
 inc_forward_mark() {
-  let forward_mark=forward_mark+0x100
+  let forward_mark=forward_mark+FOINC
 }
 
 build_local_redirect() {
@@ -63,9 +63,9 @@ build_local_redirect() {
   # echo $2 proto
   # echo $3 sport
   # echo $4 dport
-  inc_forward_mark
   $IPTABLES -t nat -A NAT_${1} -p $2 --dport $3 -j MARK --set-mark $forward_mark
   $IPTABLES -t nat -A NAT_${1} -p $2 -m mark --mark ${forward_mark}/${FMASK} -j REDIRECT --to-ports $4
+  inc_forward_mark
 #  $IPTABLES -t filter -A INPUT_${1} -p $2 --dport $4 -j ACCEPT
 }
 
@@ -75,10 +75,10 @@ build_forward_rule() {
   # echo $3 sport
   # echo $4 dstip
   # echo $5 dport
-  inc_forward_mark
 #  $IPTABLES -t mangle -A MARK_${1} -p $2 --dport $3 -j MARK --set-mark $forward_mark
   $IPTABLES -t nat -A NAT_${1} -p $2 --dport $3 -j MARK --set-mark $forward_mark
   $IPTABLES -t nat -A NAT_${1} -p $2 -m mark --mark ${forward_mark}/${FMASK} -j DNAT --to $4:$5
+  inc_forward_mark
 #  $IPTABLES -t filter -A FORWARD_${1} -p $2 -d $4 --dport $5 -j ACCEPT
 }
 
@@ -86,33 +86,33 @@ build_6forward_rule() {
   $IP6TABLES -t filter -A FORWARD_${1} -p $2 -d $3 --dport $4 -j ACCEPT
 }
 
-# ( "ZONE" "NAME OF PARAM - ex: F4U" "PROTO" )
+# ( "ZONE" "NAME OF PARAM - ex: udp4forward" "PROTO" )
 parse_4forward() {
-  get_param $2
-  for fpar in $GET_PARAM
+  get_variable $2
+  for var in $get_variable_result
   do
-    parse_comma $fpar
-    build_forward_rule $1 $3 $PARSE_COMMA
+    split_variable $var
+    build_forward_rule $1 $3 $split_variable_result
   done
 }
 
-# ( "ZONE" "NAME OF PARAM - ex: F6T" "PROTO" )
+# ( "ZONE" "NAME OF PARAM - ex: tcp6forward" "PROTO" )
 parse_6forward() {
-  get_param $2
-  for fpar in $GET_PARAM
+  get_variable $2
+  for var in $get_variable_result
   do
-    parse_comma $fpar
-    build_6forward_rule $1 $3 $PARSE_COMMA
+    split_variable $var
+    build_6forward_rule $1 $3 $split_variable_result
   done
 }
 
-# ( "ZONE" "NAME OF PARAM - ex: F4UR" "PROTO" )
+# ( "ZONE" "NAME OF PARAM - ex: udp4forward" "PROTO" )
 local_4redirect() {
-  get_param $2
-  for fpar in $GET_PARAM
+  get_variable $2
+  for var in $get_variable_result
   do
-    parse_comma $fpar
-    build_local_redirect $1 $3 $PARSE_COMMA
+    split_variable $var
+    build_local_redirect $1 $3 $split_variable_result
   done
 }
 
@@ -159,14 +159,14 @@ load_zones() {
     $IP6TABLES -t filter -A INPUT      -i $indev -d $in6net -j INPUT_${zone}
     $IP6TABLES -t filter -A FORWARD    -i $indev -d $in6net -j FORWARD_${zone}
 
-    in_ports $zone "$INU" udp
-    in_ports $zone "$INT" tcp
+    in_ports $zone "$udpin" udp
+    in_ports $zone "$tcpin" tcp
 
-    local_4redirect $zone I4UR udp
-    local_4redirect $zone I4TR tcp
-    parse_4forward $zone F4U udp
-    parse_4forward $zone F4T tcp
-    parse_6forward $zone F6T tcp
+    local_4redirect $zone udprin udp
+    local_4redirect $zone tcprin tcp
+    parse_4forward $zone udp4forward udp
+    parse_4forward $zone tcp4forward tcp
+    parse_6forward $zone tcp6forward tcp
   done
 }
 
